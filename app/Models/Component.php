b1e79dab25403c7014f31917a54566348c1cb282
zaketn <specot70@gmail.com>
2024-01-14 17:50:58 +0500

    feat: Добавлено создание параметра "Итоговая стоимость" по умолчанию

--- app/Models/Component.php
+++ app/Models/Component.php
@@ -6,10 +6,23 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Str;
 
 class Component extends Model
 {
     use HasFactory;
+
+    protected static function booted() : void
+    {
+        static::created(function(Component $component) {
+            Parameter::query()->create([
+                'name' => 'Итоговая стоимость',
+                'component_id' => $component->id,
+                'slug' => Str::slug('Итоговая стоимость'),
+                'formula' => json_encode([])
+            ]);
+        });
+    }
 
     protected $fillable = [
         'id',
---
+++ database/migrations/2024_01_14_153108_add_cost_parameter_to_each_component.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Models\Component;
+use App\Models\Parameter;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Str;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        $componentWithoutPrice = Component::query()->whereDoesntHave('parameters', function (Builder $query) {
+            $query->where('name', 'Итоговая стоимость');
+        })
+            ->get();
+
+        foreach ($componentWithoutPrice as $component) {
+            Parameter::query()->create([
+                'name' => 'Итоговая стоимость',
+                'component_id' => $component->id,
+                'slug' => Str::slug('Итоговая стоимость'),
+                'formula' => json_encode([])
+            ]);
+        }
+    }
+
+    public function down(): void
+    {
+        Parameter::query()->has('component')
+            ->where('name', 'Итоговая стоимость')
+            ->delete();
+    }
+};
